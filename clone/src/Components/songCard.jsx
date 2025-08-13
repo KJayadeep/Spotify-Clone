08f5b9e712953e49457d53fe35f82b5ecb732629
K.Jaya deep <jayadeep988@gmail.com>
2025-08-13 21:13:27 +0530

    update the card

--- clone/src/Components/songCard.jsx
+++ clone/src/Components/songCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function songCard({ song,onClick,color}) {
   return (
-    <div onClick={onClick} className={`${color ? "bg-gray-700" : "hover:bg-gray-600"}  h-[56px] flex items-center  px-2 py-8 rounded-md`}>
+    <div onClick={onClick} className={`${color ? color : "hover:bg-gray-600"} h-[56px] flex items-center px-2 py-8 rounded-md`}>
       <p className="text-white font-bold w-[16px]">{song.id + 1}</p>
       <div className="flex gap-3 items-center m-5">
         <img src={song.img} alt="" className="h-[50px] rounded-md" />
